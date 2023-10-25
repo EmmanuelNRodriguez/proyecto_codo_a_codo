10254eaf398abded8eb8a3cdc6b53d9d43a19b79
Emmanuel Nicolas Rodriguez <emmanuelnrodriguez@hotmail.com>
2023-10-24 20:22:43 -0500

    return

--- js/validacion.js
+++ js/validacion.js
@@ -10,24 +10,24 @@
     
     if(nombre.value == ""){
         alert("Debe ingresar su Nombre")
-        return
+        return false
     } 
 
     //si no tiene un formato de Email sale esto
     if( !validarEmail.test(correo.value) ){  
 		alert('Ingrese un E-mail Valido');
-        return
+        return false
 	}
     // si el asunto tiene más de 50 letras o ninguna sale esto
     if(asunto.value.length > 50 || asunto.value.length == 0){   
         alert("Por favor ingrese el asunto con un limite de 50 caracteres")
-        return
+        return false
     }
 
     // si el mensaje tiene menos de 30 caracteres y más de 1000 caracteres sale este msj
     if((mensaje.value.length < 30) || (mensaje.value.length > 1000)){   
         alert("Su mensaje debe contener entre 30 y 1000 caracteres")
-        return 
+        return false
     }
     
     alert("Su Email fue enviado con exito, pronto nos contactaremos contigo.")
