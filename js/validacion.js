954978c598ad59a6099ce066df55e134423b26ca
Emmanuel Nicolas Rodriguez <emmanuelnrodriguez@hotmail.com>
2023-10-29 21:25:27 -0500

    Mejoras en formulario

--- js/validacion.js
+++ js/validacion.js
@@ -4,44 +4,88 @@ const area = document.querySelector("#area");
 const asunto = document.querySelector("#asunto");
 const mensaje = document.querySelector("#mensaje");
 
-function validarForm() {
+
+document.getElementById('form').addEventListener('submit', function (e) {
+  
+  e.preventDefault(); // Previene el envío del formulario por defecto
+  
   var validarEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
   if (nombre.value == "") {
-    alert("Ingresá tu nombre o apodo");
+    swal({
+      title: "Información faltante!",
+      text: "Debes ingresar tu nombre o apodo.",
+      icon: "warning",
+      buttons: false,
+      timer: 2500
+    })
     return false;
   }
 
   //si no tiene un formato de Email sale esto
   if (!validarEmail.test(correo.value)) {
-    alert("Ingresá un correo electrónico válido");
+    swal({
+      title: "Información erronea o faltante!",
+      text: "Ingresá un correo electrónico válido",
+      icon: "warning",
+      buttons: false,
+      timer: 2500
+    })
     return false;
   }
 
   //si no seleccionó el área de interés
   if (area.value == "") {
-    alert("Elegí el área de tu interés");
+    swal({
+      title: "Información faltante!",
+      text: "Elegí el área de tu interés",
+      icon: "warning",
+      buttons: false,
+      timer: 2500
+    })
     return false;
   }
 
   // si el asunto tiene más de 50 letras o ninguna sale esto
+  
   if (asunto.value.length == 0 || asunto.value.length > 35) {
-    alert(
-      "El asunto de tu mensaje no puede estar vacío ni puede superar los 35 caracteres."
-    );
-    return false;
+    swal({
+      title: "Atención!",
+      text: "El asunto de tu mensaje no puede estar vacío ni puede superar los 35 caracteres.",
+      icon: "warning",
+      buttons: false,
+      timer: 2500
+    })
+      return false;
   }
 
   // si el mensaje tiene menos de 30 caracteres y más de 1000 caracteres sale este msj
   if (mensaje.value.length == 0 || mensaje.value.length > 1000) {
-    alert("Tu mensaje está vacío y no puede superar los 1000 caracteres");
+    swal({
+      title: "Atención!",
+      text: "Tu mensaje está vacio o supera la cantidad de caracteres permitidos.",
+      icon: "warning",
+      buttons: false,
+      timer: 2500
+    })
     return false;
   }
 
-  alert(
-    nombre.value.toUpperCase() +
-      "\n¡Tu mensaje dirigído a " +
-      area.value.toUpperCase() +
-      " fue enviado con éxito!\nMuy pronto te enviaremos una respuesta."
-  );
-}
+    swal({
+    title: '¿Estás seguro?',
+    text: '¿Quieres enviar este formulario?',
+    icon: 'warning',
+    buttons: true
+    }).then((value) => {
+    if (value) {  
+      swal({
+        title: "Muchas gracias!",
+        text: `Tu mensaje ${nombre.value} fue enviado con exito.`,
+        icon: "success",
+        buttons: false,
+        timer: 3000
+      })
+        document.getElementById('form').submit();
+    }
+  })
+});
