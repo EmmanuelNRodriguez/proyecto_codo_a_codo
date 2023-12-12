ff2889c76d2f7991769222e58ad19ec26a4e4c4b
leomotto <leomotto@gmail.com>
2023-12-12 15:17:01 -0300

    validación de asunto y nombre
    se incorporan modificaciones a la validacion de los campos asunto y nombre, y se modifican mensajes de swal ante errores en los inputs

--- js/validacion.js
+++ js/validacion.js
@@ -9,65 +9,73 @@ document.getElementById('form').addEventListener('submit', function (e) {
   
   e.preventDefault(); // Previene el envío del formulario por defecto
   
-  var validarEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  var validarEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var validarNombre = /^[A-Za-z\s]+$/;
 
-  if (nombre.value == "") {
+  var validarAsunto = /^(?!.*[^A-Za-z0-9\s].*|[A-Za-z0-9\s].*[^A-Za-z0-9\s].*)[A-Za-z0-9\s!@#$%^&*()_+{}\[\]:;<>,.?~\\/]+$/;
+
+  if (nombre.value == "" || !validarNombre.test(nombre.value)) {
     swal({
-      title: "Información faltante!",
-      text: "Debes ingresar tu nombre o apodo.",
+      title: "¡Información faltante!",
+      text: "Ingresar nombre o apodo",
       icon: "warning",
       buttons: false,
       timer: 2500
     })
+    nombre.focus()
     return false;
   }
 
   //si no tiene un formato de Email sale esto
   if (!validarEmail.test(correo.value)) {
     swal({
-      title: "Información erronea o faltante!",
+      title: "¡Información erronea o faltante!",
       text: "Ingresá un correo electrónico válido",
       icon: "warning",
       buttons: false,
       timer: 2500
     })
+    correo.focus()
     return false;
   }
 
   //si no seleccionó el área de interés
-  if (area.value == "") {
+  if (area.value == "default") {
     swal({
-      title: "Información faltante!",
+      title: "¡Información faltante!",
       text: "Elegí el área de tu interés",
       icon: "warning",
       buttons: false,
       timer: 2500
     })
+    area.focus()
     return false;
   }
 
   // si el asunto tiene más de 50 letras o ninguna sale esto
   
-  if (asunto.value.length == 0 || asunto.value.length > 35) {
+  if (asunto.value.length == 0 || asunto.value.length > 35 || !validarAsunto.test(asunto.value)) {
     swal({
-      title: "Atención!",
-      text: "El asunto de tu mensaje no puede estar vacío ni puede superar los 35 caracteres.",
+      title: "¡Atención!",
+      text: "El asunto de tu mensaje no puede estar vacío, no puede superar los 35 caracteres",
       icon: "warning",
       buttons: false,
       timer: 2500
     })
-      return false;
+    asunto.focus()
+    return false;
   }
 
   // si el mensaje tiene menos de 30 caracteres y más de 1000 caracteres sale este msj
   if (mensaje.value.length == 0 || mensaje.value.length > 1000) {
     swal({
-      title: "Atención!",
-      text: "Tu mensaje está vacio o supera la cantidad de caracteres permitidos.",
+      title: "¡Atención!",
+      text: "Tu mensaje está vacio o supera la cantidad de caracteres permitidos",
       icon: "warning",
       buttons: false,
       timer: 2500
     })
+    mensaje.focus()
     return false;
   }
 
@@ -80,7 +88,7 @@ document.getElementById('form').addEventListener('submit', function (e) {
     if (value) {  
       swal({
         title: "Muchas gracias!",
-        text: `Tu mensaje ${nombre.value} fue enviado con exito.`,
+        text: `Tu mensaje ${nombre.value} fue enviado con exito`,
         icon: "success",
         buttons: false,
         timer: 3000
