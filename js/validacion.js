24be0d1cefc64b1d5d1b2e7a1c11a36782c112ae
leomotto <leomotto@gmail.com>
2023-12-12 15:29:59 -0300

    Update en validacion.js
    Modificaciones en los mensajes de alert

--- js/validacion.js
+++ js/validacion.js
@@ -16,8 +16,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
 
   if (nombre.value == "" || !validarNombre.test(nombre.value)) {
     swal({
-      title: "¡Información faltante!",
-      text: "Ingresar nombre o apodo",
+      title: "Ingresá tu nombre o apodo",
+      text: "Solo se permiten letras",
       icon: "warning",
       buttons: false,
       timer: 2500
@@ -29,8 +29,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
   //si no tiene un formato de Email sale esto
   if (!validarEmail.test(correo.value)) {
     swal({
-      title: "¡Información erronea o faltante!",
-      text: "Ingresá un correo electrónico válido",
+      title: "Ingresá un correo electronico válido",
+      text: "Revisa el correo electronico ingresado",
       icon: "warning",
       buttons: false,
       timer: 2500
@@ -42,8 +42,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
   //si no seleccionó el área de interés
   if (area.value == "default") {
     swal({
-      title: "¡Información faltante!",
-      text: "Elegí el área de tu interés",
+      title: "Selecciona un area de interés",
+      text: "Debes seleccionar un area de la lista",
       icon: "warning",
       buttons: false,
       timer: 2500
@@ -56,8 +56,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
   
   if (asunto.value.length == 0 || asunto.value.length > 35 || !validarAsunto.test(asunto.value)) {
     swal({
-      title: "¡Atención!",
-      text: "El asunto de tu mensaje no puede estar vacío, no puede superar los 35 caracteres",
+      title: "Revisa el asunto de tu mensaje",
+      text: "El asunto no puede estar vacío ni superar los 35 caracteres (solo se permiten letras y números)",
       icon: "warning",
       buttons: false,
       timer: 2500
@@ -69,8 +69,8 @@ document.getElementById('form').addEventListener('submit', function (e) {
   // si el mensaje tiene menos de 30 caracteres y más de 1000 caracteres sale este msj
   if (mensaje.value.length == 0 || mensaje.value.length > 1000) {
     swal({
-      title: "¡Atención!",
-      text: "Tu mensaje está vacio o supera la cantidad de caracteres permitidos",
+      title: "Mensaje vacío",
+      text: "El cuerpo de tu mensaje está vacío o supera el límite de 1000 caracteres",
       icon: "warning",
       buttons: false,
       timer: 2500
@@ -80,15 +80,15 @@ document.getElementById('form').addEventListener('submit', function (e) {
   }
 
     swal({
-    title: '¿Estás seguro?',
-    text: '¿Quieres enviar este formulario?',
+    title: 'Mensaje listo para ser enviado',
+    text: '¿Quieres enviar tu mensaje?',
     icon: 'warning',
     buttons: true
     }).then((value) => {
     if (value) {  
       swal({
-        title: "Muchas gracias!",
-        text: `Tu mensaje ${nombre.value} fue enviado con exito`,
+        title: "¡Muchas gracias!",
+        text: `Tu mensaje ${nombre.value} ha sido enviado con éxito`,
         icon: "success",
         buttons: false,
         timer: 3000
